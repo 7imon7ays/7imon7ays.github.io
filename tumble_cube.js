d08d1cc9b590fd11dc42a7cef700fd348d30468a
Simon Chaffetz <simis1000@gmail.com>
2014-09-18 12:46:11 -0700

    Override all with tumble cube

---
+++ tumble_cube.js
@@ -0,0 +1,40 @@
+var $window = $(window);
+var $cube = $('.cube');
+
+var vertiScrolling = 0;
+var horiScrolling = 0;
+var lastScrollTop = 0;
+var lastScrollLeft = 0;
+
+$window.on('scroll', tumbleCube);
+$(window).on('resize', resize);
+
+function tumbleCube () {
+  var st = $window.scrollTop();
+  var sl = $window.scrollLeft();
+
+  if (st > lastScrollTop) {
+    vertiScrolling = vertiScrolling + 5;
+  } else if (st < lastScrollTop) {
+    vertiScrolling = vertiScrolling - 5;
+  }
+
+
+  if (sl > lastScrollLeft) {
+    horiScrolling = horiScrolling + 5;
+  } else if (sl < lastScrollLeft) {
+    horiScrolling = horiScrolling - 5;
+  }
+
+  $cube[0].style.webkitTransform = "rotateX("+ vertiScrolling +"deg) rotateY("+ horiScrolling +"deg)";
+  lastScrollTop = st;
+  lastScrollLeft = sl;
+}
+
+function resize(event) {
+  var y = ($(window).height() - 240) * 0.5;
+  $('.cube').css('margin-top', y+'px');
+}
+
+$(document).ready(resize);
+
