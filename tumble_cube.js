5e75990d0d6408c1f5e84913ae96fcb102d6620a
Simon Chaffetz <simis1000@gmail.com>
2014-09-18 23:38:03 -0700

    Make edge length global variable

--- tumble_cube.js
+++ tumble_cube.js
@@ -1,10 +1,11 @@
-var $window = $(window);
-var $cube = $('.cube');
+var $window = $(window),
+    $cube = $('.cube'),
+    $face = $('.face');
 
-var vertiScrolling = 0;
-var horiScrolling = 0;
-var lastScrollTop = 0;
-var lastScrollLeft = 0;
+var vertiScrolling = 0,
+    horiScrolling = 0,
+    lastScrollTop = 0,
+    lastScrollLeft = 0;
 
 $window.on('scroll', tumbleCube);
 $window.on('resize', resize);
@@ -31,9 +32,39 @@ function tumbleCube () {
   lastScrollLeft = sl;
 }
 
+var edgeLength = 360,
+    $one = $('.one'),
+    $two = $('.two'),
+    $three = $('.three');
+    $four = $('.four'),
+    $five = $('.five'),
+    $six = $('.six');
+
+
 function resize(event) {
-  var y = ($(window).height() - 240) * 0.5;
-  $('.cube').css('margin-top', y+'px');
+  var edgeLength = 360,
+      halfLength = edgeLength / 2,
+      quarterLength = halfLength / 2,
+      topMargin= ($window.height() - edgeLength) * 0.5;
+
+  $cube.css('margin-top', topMargin + 'px');
+  $cube.css('height', edgeLength + 'px');
+  $face.css('height', edgeLength + 'px');
+  $cube.css('width', edgeLength + 'px');
+
+  $one.css('-webkit-transform',
+      'rotateX(' + quarterLength + 'deg) translateZ(' + halfLength + 'px)');
+  $two.css('-webkit-transform',
+      'translateZ(' + halfLength + 'px)');
+  $three.css('-webkit-transform',
+     'rotateY(' + quarterLength + 'deg) translateZ(' + halfLength + 'px)');
+  $four.css('-webkit-transform',
+      'rotateY(' + halfLength + 'deg) translateZ(' + halfLength + 'px)');
+  $five.css('-webkit-transform',
+      'rotateY(-' + quarterLength + 'deg) translateZ(' + halfLength + 'px)');
+  $six.css('-webkit-transform',
+      'rotateX(-' + quarterLength + 'deg) translateZ(' + halfLength +
+        'px) rotate(' + halfLength + 'deg)');
 }
 
 $(document).ready(resize);
