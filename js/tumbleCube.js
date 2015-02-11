27d5d9f66775959c863895dafa51297670fec867
Simon Chaffetz <simis1000@gmail.com>
2015-02-10 21:48:38 -0800

    Add media queries for font size and title screen on mobile

--- js/tumbleCube.js
+++ js/tumbleCube.js
@@ -322,14 +322,14 @@ $window.on('resize', resize);
       if (event.target.tagName == 'A') return;
 
       event.preventDefault();
-      var touch = event.touches[0];
+      var touch = (event.touches ? event.touches[0] : event.detail.touches[0] );
       lastx = touch.pageX;
       lasty = touch.pageY;
   }, false);
 
   document.addEventListener('touchmove', function(event) {
       event.preventDefault();
-      var touch = event.touches[0];
+      var touch = (event.touches ? event.touches[0] : event.detail.touches[0] );
       var mousex = touch.pageX;
       var mousey = touch.pageY;
       if (lastx !== mousex) vx = mousex - lastx;
@@ -359,6 +359,26 @@ $window.on('resize', resize);
 function isMobile () {
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
       navigator.userAgent);
+}
+
+function titlCubeMobile () {
+  var dragEvents = [
+        new CustomEvent("touchstart"),
+        new CustomEvent("touchmove"),
+        new CustomEvent("touchend")
+  ];
+
+  dragEvents[0].initCustomEvent("touchstart", true, true, {
+    touches: [{ pageX: 0, pageY: 0 }]
+  });
+
+  dragEvents[1].initCustomEvent("touchmove", true, true, {
+    touches: [{ pageX: 11, pageY: 6 }]
+  });
+
+  for (var i = 0; i < dragEvents.length; i++) {
+    document.dispatchEvent(dragEvents[i]);
+  }
 }
 
 ;function tumbleCubeDesktop($cube, $window) {
@@ -456,6 +476,14 @@ function isMobile () {
   }
 }
 
+function titlCubeDesktop () {
+  var offsetScroll = $.Event("mousewheel", {
+    deltaX: -90, deltaY: -10, which: 1
+  });
+
+  $window.trigger(offsetScroll);
+}
+
 ;function tumbleNav () {
   var $navlist = $(".nav-list"),
       $flash = $(".flash");
@@ -492,15 +520,12 @@ function isMobile () {
 
   if (isMobile()) {
     tumbleCubeMobile($cube);
+    titlCubeMobile();
   } else {
     tumbleCubeDesktop($cube, $window);
+    titlCubeDesktop();
   }
 
-  var offsetScroll = $.Event("mousewheel", {
-    deltaX: -90, deltaY: -10, which: 1
-  });
-
-  $window.trigger(offsetScroll);
   tumbleNav();
 }());
 
